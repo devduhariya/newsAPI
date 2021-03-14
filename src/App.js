76d24e91a3e5ffdb0b8f9732cf962899329c9167
devduhariya <64675676+devduhariya@users.noreply.github.com>
2021-03-14 11:46:14 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Newsdata from './newData';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <Newsdata/>
+    </div>
+  );
+}
+
+export default App;
